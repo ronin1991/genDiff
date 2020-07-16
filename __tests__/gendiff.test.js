8b64e00f74ff2f7e9b38ee7e8105c02d20c40c05
ronin1991 <ronin1991@rambler.ru>
2020-07-16 20:15:06 +0300

    fix test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-underscore-dangle */
-import { test, expect } from '@jest/globals';
+import { test, expect, describe } from '@jest/globals';
 import fs from 'fs';
 import { fileURLToPath } from 'url';
 import path from 'path';
@@ -9,49 +9,19 @@ import gendiff from '../src';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
 const getFixturePath = (fileName) => path.join(__dirname, '__fixtures__', fileName);
 
+const readFile = (format) => fs.readFileSync(getFixturePath(`${format}.txt`), 'utf-8');
 
-const firstJson = getFixturePath('first.json');
-const secondJson = getFixturePath('second.json');
+const filesByExtension = (extensions) => extensions
+  .map((extension) => [`${getFixturePath(`first${extension}`)}`, `${getFixturePath(`second${extension}`)}`]);
 
-
-const firstYaml = getFixturePath('first.yaml');
-const secondYaml = getFixturePath('second.yaml');
-
-const firstIni = getFixturePath('first.ini');
-const secondIni = getFixturePath('second.ini');
+const formats = ['json', 'default', 'plain'];
+const extensions = ['.json', '.ini', '.yaml'];
 
 
-const outDefault = fs.readFileSync(getFixturePath('default.txt'), 'utf-8');
-const outPlain = fs.readFileSync(getFixturePath('plain.txt'), 'utf-8');
-const outJson = fs.readFileSync(getFixturePath('out.txt'), 'utf-8');
-
-test.each([
-  [firstJson, secondJson, outDefault],
-  [firstYaml, secondYaml, outDefault],
-  [firstIni, secondIni, outDefault],
-])('outDefault(%s, %s)', (firstFile, secondFile, expected) => {
-  expect(gendiff(firstFile, secondFile, 'default')).toBe(expected);
+describe.each(formats)('Test %s format', (format) => {
+  test.each(filesByExtension(extensions))('should work with %s ', (first, second) => {
+    expect(gendiff(first, second, format)).toBe(readFile(format));
+  });
 });
-
-
-test.each([
-  [firstJson, secondJson, outPlain],
-  [firstYaml, secondYaml, outPlain],
-  [firstIni, secondIni, outPlain],
-])('outIni(%s, %s)', (firstFile, secondFile, expected) => {
-  expect(gendiff(firstFile, secondFile, 'plain')).toBe(expected);
-});
-
-test.each([
-  [firstJson, secondJson, outJson],
-  [firstYaml, secondYaml, outJson],
-  [firstIni, secondIni, outJson],
-])('outJson(%s, %s)', (firstFile, secondFile, expected) => {
-  expect(gendiff(firstFile, secondFile, 'json')).toBe(expected);
-});
-
-// describe.each(formats)('Test %s format', (format) => {
-//   it.each(filesByExtension)('should work with %s extension', (_, before, after) => {}}))
