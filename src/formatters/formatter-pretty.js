0fa10af55d2313b7f0dce04c8500d9e78d0a0644
ronin1991 <ronin1991@rambler.ru>
2020-10-01 20:12:13 +0300

    refactoring formater-pretty.js

--- src/formatters/formatter-pretty.js
+++ src/formatters/formatter-pretty.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 
-const TAB = '  ';
+const TAB = '    ';
 const getIndent = (number) => TAB.repeat(number);
 
 const stringify = (nodeValue, depth) => {
@@ -9,27 +9,27 @@ const stringify = (nodeValue, depth) => {
   }
 
   const string = Object.entries(nodeValue)
-    .map(([key, value]) => `{\n${getIndent(depth + 3)}${key}: ${stringify(value, depth + 3)}\n${getIndent(depth + 1)}}`);
+    .map(([key, value]) => `{\n${getIndent(depth + 1)}${key}: ${stringify(value, depth + 1)}\n${getIndent(depth)}}`);
 
   return string;
 };
 
-const renderDefault = (ast, depth = 1) => ast
+const renderDefault = (ast, depth = 0) => ast
   .map((node) => {
-    const getValue = (value) => `${node.key}: ${stringify(value, depth)}`;
+    const getValue = (value) => `${node.key}: ${stringify(value, depth + 1)}`;
     const indent = getIndent(depth);
 
     switch (node.type) {
       case 'added':
-        return `${indent}+ ${getValue(node.value)}`;
+        return `${indent}  + ${getValue(node.value)}`;
       case 'deleted':
-        return `${indent}- ${getValue(node.value)}`;
+        return `${indent}  - ${getValue(node.value)}`;
       case 'unchanged':
-        return `${indent}  ${getValue(node.value)}`;
+        return `${indent}    ${getValue(node.value)}`;
       case 'changed':
-        return `${indent}- ${getValue(node.value.oldValue)}\n${indent}+ ${getValue(node.value.newValue)}`;
+        return `${indent}  - ${getValue(node.value.oldValue)}\n${indent}  + ${getValue(node.value.newValue)}`;
       case 'nested':
-        return `${indent}  ${node.key}: {\n${renderDefault(node.children, depth + 2)}\n${getIndent(depth + 1)}}`;
+        return `${indent}    ${node.key}: {\n${renderDefault(node.children, depth + 1)}\n${getIndent(depth + 1)}}`;
       default:
         throw new Error(`no parser for this ${node.type}`);
     }
